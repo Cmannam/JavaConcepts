ae053227acdfadabc335f8a3a7db3f9555032805
Cmannam <chandana.mannam@dxc.com>
2022-02-15 16:29:17 +0530

    Intial Commit

---
+++ src/com/dxc/strings/Buffer.java
@@ -0,0 +1,16 @@
+package com.dxc.strings;
+
+import java.io.*;
+class GFG {
+    public static void main(String[] args)
+    {
+        StringBuffer s = new StringBuffer("SAP");
+        s.append("Habris");
+        System.out.println(s);
+        s.append(1);
+        System.out.println(s);
+        s.append("Commerce");
+        System.out.println(s);
+
+    }
+}
