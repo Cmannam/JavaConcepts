dc943191216a8fc57e8b242275a24c8ad4408a32
Cmannam <chandana.mannam@dxc.com>
2022-02-15 17:14:08 +0530

    Intial Commit

--- src/com/dxc/DataStructures/ArrayList1.java
+++ src/com/dxc/DataStructures/ArrayList1.java
@@ -1,26 +1,26 @@
-package com.dxc.Collections;
+package com.dxc.DataStructures;
 
 import java.util.ArrayList;
 import java.util.Iterator;
-public class Demo {
-    public static void main(String args[]){
+public class ArrayList1 {
+    public static void main(String args[])
+    {
         ArrayList<String> List1 = new ArrayList<String>();
         ArrayList List2 = new ArrayList();
-        List1.add("Rakshitha");
-        List1.add("Soumya");
-        List1.add("Chandana");
-        List2.add("Arpita");
-        List2.add("Nandhini");
-        List2.add("Prajwala");
-        List2.add(123);
+        List1.add("Apple");
+        List1.add("Mango");
+        List1.add("Pineapple");
+        List2.add("Tulip");
+        List2.add("Jasmine");
+        List2.add("Daisy");
         System.out.println("List1 is "+List1);
         System.out.println("List2 is "+List2);
-        String name = List1.get(2);
+        String name = List1.get(1);
         System.out.println("name is "+ name);
-        List2.remove(3);
+        List2.remove(2);
         System.out.println("List2 is "+ List2);
-        if(List1.contains("Soumya")){
-            System.out.println("Soumya is in list");
+        if(List1.contains("Mango")){
+            System.out.println("Mango is in list");
         }
         for(String str:List1)
         {
@@ -31,7 +31,7 @@ public class Demo {
         while (itr.hasNext()){
             String str = itr.next();
             System.out.println(str);
-            if(str.equals("Rakshitha"))
+            if(str.equals("Apple"))
             {
                 itr.remove();
             }
@@ -39,3 +39,6 @@ public class Demo {
         System.out.println("now List1 is "+List1);
     }
 }
+
+
+
